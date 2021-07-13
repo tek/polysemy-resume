ea38a91202d014afa941a0671012de796f69967b
Torsten Schmits <torstenschmits@gmail.com>
2021-07-13 02:58:15 +0200

    use polysemy 1.6

--- flake.nix
+++ flake.nix
@@ -1,38 +1,37 @@
 {
   description = "Polysemy Error Tracking";
 
-  inputs = {
-    hix.url = github:tek/hix;
-    polysemy.url = github:polysemy-research/polysemy;
-  };
+  inputs.hix.url = github:tek/hix;
 
-  outputs = { hix, polysemy, ... }:
+  outputs = { hix, ... }:
   let
     compat901 = { hackage, source, jailbreak, minimal, noHpack, ... }: {
-      path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
-      path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
-      polysemy = noHpack (minimal (source.root polysemy));
-      polysemy-plugin = noHpack (minimal (source.sub polysemy "polysemy-plugin"));
-      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
       type-errors-pretty = jailbreak;
     };
 
     compat = { hackage, ... }: {
-      polysemy = hackage "1.5.0.0" "1xl472xqdxnp4ysyqnackpfn6wbx03rlgwmy9907bklrh557il6d";
-      polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
-      polysemy-test = hackage "0.3.1.4" "093vxf6i78a3fghn2fwgxdj9y59272q9i9px0315wg17xrg80kh6";
+      polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
+      polysemy-plugin = hackage "0.4.0.0" "0pah1a8h8ckbv2fq20hrikrd1p5a3bdxr03npkyixc6mv5k1rmck";
+      polysemy-test = hackage "0.3.1.6" "0bfh37l68a5chhjfr7gqcffsmvdgg5hqclxi0fc5xnqni2mg81ak";
+    };
+
+    common = { hackage, jailbreak, ... }: {
+      path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
+      path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
+      polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
+      polysemy-plugin = hackage "0.4.0.0" "0pah1a8h8ckbv2fq20hrikrd1p5a3bdxr03npkyixc6mv5k1rmck";
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
 
     main = { hackage, ... }: {
       tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
-      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
   in
   hix.flake {
     base = ./.;
-    packages.polysemy-resume = "packages/resume";
-    overrides = [compat main];
-    compatOverrides = { all = compat; ghc901 = compat901; };
+    packages.polysemy-resume = ./packages/resume;
+    overrides = [compat common main];
+    compatOverrides = { all = compat; ghc901 = [common compat901]; };
     versionFile = "ops/hpack/shared/meta.yaml";
   };
 }
